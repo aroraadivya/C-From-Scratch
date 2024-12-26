c354d35bd8daf007da466b984dd207ec43244b0d
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-26 15:38:00 +0530

    Refactor prime number check to use a loop for better accuracy

--- primeOrNot.cpp
+++ primeOrNot.cpp
@@ -5,10 +5,14 @@ int main(){
     int n;
     cin>>n;
 
-    if(n%2==0){
-        cout<<"Not a prime number"<<endl;
-    }
-    else{
-        cout<<"Prime number"<<endl;
+    int i=2;
+    while(i<n){
+        if(n%i==0){
+            cout<<"Not a prime number."<<endl;
+        }
+        else{
+            cout<<"Prime number."<<endl;
+        }
+        i += 1;
     }
 }
