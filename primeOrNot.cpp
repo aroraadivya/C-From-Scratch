c0e3e9b0f4d7b8bfc5d3b803bd3c1ee513482890
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-26 15:03:33 +0530

    Add program to check if a number is prime or not

---
+++ primeOrNot.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    if(n%2==0){
+        cout<<"Not a prime number"<<endl;
+    }
+    else{
+        cout<<"Prime number"<<endl;
+    }
+}
